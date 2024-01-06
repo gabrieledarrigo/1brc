f63d3dcc91296d798da4eb1c395f3acbcd6ede1f
Gabriele D'Arrigo <darrigo.g@gmail.com>
2024-01-06 14:27:19 +0100

    Implement a measurement structure

--- src/main.rs
+++ src/main.rs
@@ -20,6 +20,7 @@
 // {Abha=-23.0/18.0/59.2, Abidjan=-16.2/26.0/67.3, Abéché=-10.0/29.4/69.0, Accra=-10.1/26.4/66.4, Addis Ababa=-23.7/16.0/67.0, Adelaide=-27.8/17.3/58.5, ...}
 
 use std::{
+    collections::HashMap,
     fs::File,
     io::{BufRead, BufReader},
     process::exit,
@@ -27,8 +28,20 @@ use std::{
 
 const MEASUREMENT_FILE: &str = "./measurements.txt";
 
-fn calculate_average(station: &str, measurement: f32) {
+fn calculate_average(
+    station: &str,
+    measurement: f32,
+    measurements: &mut HashMap<String, Measurement>,
+) {
     println!("Stations is {}, measurement is {}", station, measurement);
+
+    let station = measurements
+        .entry(station.to_string())
+        .or_insert(Measurement::default());
+
+    station.update(measurement);
+
+    // stations.
 }
 
 fn read_measurement_file() -> Option<impl BufRead> {
@@ -47,7 +60,36 @@ fn parse_line(line: &str) -> Option<(String, f32)> {
     })
 }
 
+pub struct Measurement {
+    min: f32,
+    max: f32,
+    sum: f32,
+    count: i32,
+}
+
+impl Default for Measurement {
+    fn default() -> Self {
+        Self {
+            min: f32::MAX,
+            max: f32::MIN,
+            sum: 0.0,
+            count: 0,
+        }
+    }
+}
+
+impl Measurement {
+    pub fn update(&mut self, measurement: f32) {
+        self.min = self.min.min(measurement);
+        self.max = self.max.max(measurement);
+        self.count = self.count + 1;
+        self.sum = self.sum + measurement;
+    }
+}
+
 fn main() {
+    let mut map: HashMap<String, Measurement> = HashMap::new();
+
     let buffer = read_measurement_file().unwrap_or_else(|| {
         println!("Cannot open {}", MEASUREMENT_FILE);
         exit(1);
@@ -67,30 +109,50 @@ fn main() {
             exit(1);
         });
 
-        calculate_average(&station, measurement);
+        calculate_average(&station, measurement, &mut map);
     }
 }
 
 #[cfg(test)]
 mod tests {
-    use crate::parse_line;
+    use crate::{parse_line, Measurement};
 
     #[test]
     fn test_parse_line() {
-        let line = "Milan;13.0";
-
-        let (station, measurement) = parse_line(line).unwrap();
-
-        assert_eq!(station, "Milan");
-        assert_eq!(measurement, 13.0);
+        assert_eq!(
+            parse_line("Bulawayo;8.9"),
+            Some(("Bulawayo".to_string(), 8.9))
+        );
+        assert_eq!(
+            parse_line("St. John's;15.2"),
+            Some(("St. John's".to_string(), 15.2))
+        );
+        assert_eq!(
+            parse_line("Istanbul;6.2"),
+            Some(("Istanbul".to_string(), 6.2))
+        );
+        assert_eq!(parse_line("Invalid Line"), None);
     }
 
     #[test]
-    fn test_parse_line_returns_none() {
-        let line = "Milan 13.0";
+    fn test_update() {
+        let mut measurement = Measurement::default();
+        measurement.update(10.0);
+        assert_eq!(measurement.min, 10.0);
+        assert_eq!(measurement.max, 10.0);
+        assert_eq!(measurement.count, 1);
+        assert_eq!(measurement.sum, 10.0);
 
-        let actual = parse_line(line);
+        measurement.update(5.0);
+        assert_eq!(measurement.min, 5.0);
+        assert_eq!(measurement.max, 10.0);
+        assert_eq!(measurement.count, 2);
+        assert_eq!(measurement.sum, 15.0);
 
-        assert!(actual.is_none());
+        measurement.update(15.0);
+        assert_eq!(measurement.min, 5.0);
+        assert_eq!(measurement.max, 15.0);
+        assert_eq!(measurement.count, 3);
+        assert_eq!(measurement.sum, 30.0);
     }
 }
